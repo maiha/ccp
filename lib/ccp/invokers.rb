90e485e1f2a5092755e0bac2216e05b4fd1b2305
maiha <maiha@wota.jp>
2012-05-08 16:09:09 +0900

    Refactor invoker with Options

--- lib/ccp/invokers.rb
+++ lib/ccp/invokers.rb
@@ -1,5 +1,6 @@
 module Ccp
   module Invokers
     autoload :Base           , 'ccp/invokers/base'
+    autoload :Options        , 'ccp/invokers/options'
   end
 end
---
+++ lib/ccp/invokers/options.rb
@@ -0,0 +1,49 @@
+module Ccp
+  module Invokers
+    module Options
+      def self.included(base)
+        super
+
+        class << base
+          def options(*keys)
+            Proxy.new(self, *keys)
+          end
+        end
+      end
+
+      class Proxy
+        include Enumerable
+
+        attr_reader :keys
+
+        def initialize(base, *keys)
+          @base = base
+          @keys = keys.map(&:to_sym)
+
+          @keys.each do |key|
+            @base.dsl_accessor key
+          end
+        end
+
+        def [](key)
+          @base.__send__(key)
+        end
+
+        def each(&block)
+          keys.each do |key|
+            yield(self[key])
+          end
+        end
+
+        def options
+          opts = {}
+          keys.each do |key|
+            val = self[key]
+            opts[key] = val unless val.nil?
+          end
+          return opts
+        end
+      end
+    end
+  end
+end
